c329792b6dfa6d98dcad9bf487e50ba175191a62
eknox22 <128257305+eknox22@users.noreply.github.com>
2024-10-06 18:28:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import requests
 from rich import print
 from datetime import datetime
 
+def display_temperature(day, temperature, unit='C'):
+    print(f"[blue]{day}[/blue]: {round(temperature)}º{unit}")
+
+
 def display_current_weather(city):
     api_key = "t61a3de96178c340b47o7ff32962bb7f"
     api_url = f"https://api.shecodes.io/weather/v1/current?query={city}&key={api_key}"
 
     response = requests.get(api_url)
     current_weather_data = response.json()
+    print("\n[green bold]Forecast:[/green bold]")
     current_weather_city = current_weather_data['city']
     current_weather_temperature = current_weather_data['temperature']['current']
 
-    print(f"Today: {round(current_weather_temperature)}ºC")
+    display_temperature("Today", round(current_weather_temperature))
 
 def display_forecast_weather(city_name):
     api_key = "t61a3de96178c340b47o7ff32962bb7f"
@@ -28,16 +33,23 @@ def display_forecast_weather(city_name):
         temperature = day['temperature']['day']
         
         if date.date() != datetime.today().date():
-        print(f"{formatted_day}: {round(temperature)}ºC")
+            display_temperature(formatted_day, round(temperature))
+        
+def credit():
+    print("\n[blue bold]This app was built by Emily Knox-Clifton[blue bold]")
+    
+def welcome():
+    print("[purple bold]Welcome to my weather app[/purple bold]")
 
 
-city_name = input('Enter a city: ')
+welcome()
+city_name = input("Enter a city: ")
 city_name = city_name.strip()
 
 if city_name:
     display_current_weather(city_name)
-    
-    print('\n[green bold]Forecast: [/green bold]')
     display_forecast_weather(city_name)
+    credit()
 else:
-    print('Please try again with a city.')
+      print("Please try again with a city")
+
